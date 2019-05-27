7704cb211372b814f7fc97791bb688ead02e71c2
ciechaojun <184613486@qq.com>
2019-05-27 21:54:27 +0800

    oauth

--- vue/vue-demo/element/src/components/Login.vue
+++ vue/vue-demo/element/src/components/Login.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class='login'>
-    <div class='container'>
-      <p class='title'>后台管理系统</p>
-      <div class='msg mt20'>
-        <el-input v-model='user' placeholder='请输入账号'></el-input>
+  <div class="login">
+    <div class="container">
+      <p class="title">后台管理系统</p>
+      <div class="msg mt20">
+        <el-input v-model="user" placeholder="请输入账号"></el-input>
       </div>
-      <div class='msg mt20'>
-        <el-input v-model='password' placeholder='请输入密码'></el-input>
+      <div class="msg mt20">
+        <el-input v-model="password" placeholder="请输入密码"></el-input>
       </div>
-      <div class='btn mt20'>
-        <el-button type='primary' v-on:click='login'>主要按钮</el-button>
+      <div class="btn mt20">
+        <el-button type="primary" v-on:click="login">主要按钮</el-button>
         <!-- <router-link to='./HelloWorld'>
           <el-button type='primary' @click='login'>主要按钮</el-button>
-        </router-link> -->
+        </router-link>-->
       </div>
     </div>
   </div>
@@ -21,19 +21,19 @@
 <script>
 /* eslint-disable no-new */
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
-      user: '',
-      password: ''
-    }
+      user: "",
+      password: ""
+    };
   },
   methods: {
     login() {
       if (this.user && this.password) {
-        localStorage.setItem('userName',this.user)
-        // 路由跳转  
-        this.$router.push('/hello') //字符串
+        localStorage.setItem("userName", this.user);
+        // 路由跳转
+        this.$router.push("/hello"); //字符串
         // this.$router.push({path:'/hello'})  // 对象
         // this.$router.push({name:'HelloWorld',param:{userId:1}})
       }
@@ -47,11 +47,11 @@ export default {
   width: 100%;
 }
 .container {
-  width: 300%;
+  width: 300px;
   padding: 50px;
   border-radius: 4px;
   margin: 0 auto;
-  padding-top: 100px;
+  margin-top: 100px;
   box-shadow: 0 0 20px #cac6c6;
 }
 .mt20 {
@@ -61,7 +61,7 @@ export default {
   width: 100%;
 }
 .title {
-  font-size: 16px;
+  font-size: 26px;
   font-weight: bold;
   color: #505458;
 }
