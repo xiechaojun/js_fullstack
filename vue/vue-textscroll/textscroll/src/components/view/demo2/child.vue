ac3d07857243bea7e444798839b2adaebb850e30
ciechaojun <184613486@qq.com>
2019-06-11 21:48:19 +0800

    vue-textscroll

---
+++ vue/vue-textscroll/textscroll/src/components/view/demo2/child.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <button @click="sendMsg">ok</button>
+  </div>
+</template>
+
+<script>
+import Child from './child'
+export default {
+  name:'demoChild2',
+  data() {
+    return {
+      message:'akf'
+    }
+  },
+  methods: {
+    sendMsg(){
+      this.$emit('wn',message)
+    }
+  },
+};
+</script>
---
+++ vue/vue-textscroll/textscroll/src/components/view/demo2/father.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    {{msg}}
+    <child @wn='getWn'></child>
+  </div>
+</template>
+
+<script>
+import child from "./child";
+export default {
+  name: "demoChild2",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    child
+  },
+  methods: {
+    getWn (data) {
+      console.log(data)
+      this.msg = data
+    }
+  },
+};
+</script>
