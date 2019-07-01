d975fc4a2423bcf2216875d3b4800078aaeb4278
ciechaojun <184613486@qq.com>
2019-07-01 17:08:58 +0800

    new this call bind apply

--- react/00 BoilerPlate/src/components/header.tsx
+++ react/00 BoilerPlate/src/components/header.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-export const Header = () => {
+
+export const Header:React.StatelessComponent = () => {
   return (
-    <div>Header</div>
+    <div className="row">
+      <h2>Application Header</h2>
+    </div>
   )
 }
