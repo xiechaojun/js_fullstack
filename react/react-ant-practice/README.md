6a1b54c705d373614b56d46c4ba10c82d3bb46c4
ciechaojun <184613486@qq.com>
2019-07-09 14:40:28 +0800

    react-ant-practice

---
+++ react/react-ant-practice/README.md
@@ -0,0 +1,7 @@
+## touchStart 按下去的时候  opacity  0.6
+## touchEnd 手抬起来的时候 opacity 1
+## ref 
+  string ref 
+  function ref 
+  object ref 
+## 受控组件 非受控组件 
---
+++ react/react-ant-practice/src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import { Button, Icon } from 'antd';
+import TouchableOpacity from './TouchableOpacity';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+
+const size = 'large';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.functionRef = null;
+    this.objectRef = React.createRef();
+    this.inputRef = React.createRef();
+  }
+  state = {
+    value:''
+  }
+  handleInputChange = (e) => {
+    let value = e.target.value;
+    value = value.slice(0,10);
+    this.setState({
+      value
+    })
+  }
+  componentDidMount(){
+    // 组件 挂载在页面上
+    // 通过原生 js 操作 DOM 
+    this.refs.strRef.innerHTML = 'String ref';
+    this.FunctionRef.innerHTML = 'Function ref';
+    this.objectRef.current.innerHTML = 'Object ref';
+    // this.FunctionRef.addEventListener('click', () => {
+    //   console.log('function ref clicked');
+    // })
+  }
+  handlePrint1 = () => {
+    console.log(this.inputRef.current.value);
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <Button type="primary">Primary</Button>
+          <Button>Default</Button>
+          <Button type="dashed">Dashed</Button>
+          <Button type="danger">Danger</Button>
+          <Button type="link">Link</Button>
+          <br />
+          <Button type="primary" size={size}>
+            Primary
+        </Button>
+          <Button size={size}>Normal</Button>
+          <Button type="dashed" size={size}>
+            Dashed
+        </Button>
+          <Button type="danger" size={size}>
+            Danger
+        </Button>
+          <Button type="link" size={size}>
+            Link
+        </Button>
+          <br />
+          <Button type="primary" shape="circle" icon="download" size={size} />
+          <Button type="primary" shape="round" icon="download" size={size}>
+            Download
+        </Button>
+          <Button type="primary" icon="download" size={size}>
+            Download
+        </Button>
+          <br />
+          <Button.Group size={size}>
+            <Button type="primary">
+              <Icon type="left" />
+              Backward
+          </Button>
+            <Button type="primary">
+              Forward
+            <Icon type="right" />
+            </Button>
+          </Button.Group>
+          <TouchableOpacity>
+            确定
+            <a href="">确定</a>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            取消
+          </TouchableOpacity>
+        </div>
+        <h1 ref="strRef"></h1>
+        <h1 ref={(node) => {
+          this.FunctionRef = node;
+        }}></h1>
+        <h1 ref={this.objectRef}></h1>
+        {/* 非受控 */}
+        <input type="text" ref={this.inputRef} />
+        <button onClick={this.handlePrint1}>btn1</button>
+        {/* 受控
+        value 
+        state 来源应该单一 value 应该来自于 state
+        */}
+        <input type="text" value={this.state.value} onChange={this.handleInputChange} />
+        <button onClick={this.handlePrint2}>btn2</button>
+      </div>
+    );
+  }
+}
+
+export default App;
