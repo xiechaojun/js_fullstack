10adab96cfbaabd5f0607ba42700ef744618ba2f
ciechaojun <184613486@qq.com>
2019-07-15 17:13:54 +0800

    beer

---
+++ react/beer/README.md
@@ -0,0 +1,13 @@
+- es6  class 新特性
+ ref = this.createRef();
+ ref = {this.ref};
+
+- history html5 API 
+search main 的子组件
+window.history
+hoc props
+<Route><search/></Route>
+
+- 高阶组件
+  能返回组件的函数，接受组件作为参数，高阶组件
+  withRouter react-router-dom 可以封装一个组件并为之提供this.props.location
---
+++ react/beer/src/components/Main.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from './Header.js'
+import Search from './Search.js'
+import Results from './Results'
+
+export default class Main extends React.Component  {
+  constructor () {
+    super();
+    this.state = {
+      beers:[],
+      loading:true
+    }
+  }
+  componentDidMount () {
+    const params = this.props.match.params || {};   //  /     /search/:searchTerm
+    const searchTerm = params.searchTerm || undefined;
+    this.loadBeers(searchTerm)
+   };
+   loadBeers (searchTerm = "hops") {
+     fetch(`http://api.react.beer/v2/search?q=${searchTerm}&type=beer`)
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+    })
+   }
+  render () {
+    return (
+      <div className="wrapper">
+        <Header siteName="Beer me!" />
+        <Search />
+        <Results beers={this.state.beers} loading={this.state.loading} />
+      </div>
+
+    )
+  }
+}
